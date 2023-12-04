696432086c99fd14a591c6335ffada9c180a2799
andreafrancioni <andreafrancioni.work@gmail.com>
2023-12-04 15:15:17 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<!-- JS App -->
+<script>
+/* Import package */
+import axios from 'axios'; //importo Axios
+import { store } from "./store.js" //state management
+
+/* Import components */
+
+
+export default {
+  components: {
+
+  },
+
+  data() {
+    return {
+      /* Richiama store per utilizzare il file store.js come state management */
+      store,
+    }
+  },
+  mounted() {
+    /* All'avvio dell'app lancia tali funzioni */
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<!-- HTML App -->
+<template>
+  <header>
+
+  </header>
+  <main>
+
+  </main>
+  <footer>
+
+  </footer>
+</template>
+
+<!-- CSS App -->
+<style scoped></style>
